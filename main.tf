4967ba80d119005fdee7f0b633bb23c68bf1eca9
PScoriae <pierreccesario@gmail.com>
2022-11-11 23:20:47 +0800

    initial commit

---
+++ main.tf
@@ -0,0 +1,231 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.38.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+resource "cloudflare_record" "pcpartstool" {
+  zone_id = var.cloudflare_zone_id
+  name    = "pcpartstool"
+  value   = aws_eip.public-web-server-ip.public_ip
+  type    = "A"
+  proxied = true
+}
+
+resource "cloudflare_record" "jenkins" {
+  zone_id = var.cloudflare_zone_id
+  name    = "jenkins"
+  value   = aws_eip.cicd-server-ip.public_ip
+  type    = "A"
+  proxied = true
+}
+
+resource "aws_instance" "cicd-server" {
+  ami                  = "ami-094bbd9e922dc515d"
+  iam_instance_profile = "S3-Full-Access"
+  instance_type        = "t3a.small"
+  availability_zone    = "ap-southeast-1a"
+  key_name             = "main-key"
+
+  network_interface {
+    network_interface_id = aws_network_interface.cicd-server-nic.id
+    device_index         = 0
+  }
+
+  tags = {
+    Name = "CICD Server"
+  }
+}
+
+resource "aws_instance" "web-server" {
+  ami               = "ami-094bbd9e922dc515d"
+  instance_type     = "t2.micro"
+  availability_zone = "ap-southeast-1a"
+  key_name          = "main-key"
+
+  network_interface {
+    network_interface_id = aws_network_interface.web-server-nic.id
+    device_index         = 0
+  }
+
+  tags = {
+    Name = "Web Hosting Server"
+  }
+}
+
+resource "aws_vpc" "main-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_internet_gateway" "main-gateway" {
+  vpc_id = aws_vpc.main-vpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "main-route-table" {
+  vpc_id = aws_vpc.main-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main-gateway.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.main-gateway.id
+  }
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id                  = aws_vpc.main-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-southeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "subnet-1"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.main-route-table.id
+}
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.main-vpc.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow http"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh_traffic"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main-vpc.id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow ssh"
+  }
+}
+
+resource "aws_security_group" "allow_cicd_web_console" {
+  name        = "allow_cicd_web_console"
+  description = "Allow Web Consoles Ingress"
+  vpc_id      = aws_vpc.main-vpc.id
+
+  ingress {
+    description      = "Jenkins"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow cicd web consoles"
+  }
+}
+
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id, aws_security_group.allow_ssh.id]
+}
+
+resource "aws_network_interface" "cicd-server-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.60"]
+  security_groups = [aws_security_group.allow_web.id, aws_security_group.allow_ssh.id, aws_security_group.allow_cicd_web_console.id]
+}
+
+resource "aws_eip" "public-web-server-ip" {
+  instance = aws_instance.web-server.id
+  vpc      = true
+
+  depends_on = [
+    aws_internet_gateway.main-gateway
+  ]
+}
+
+resource "aws_eip" "cicd-server-ip" {
+  instance = aws_instance.cicd-server.id
+  vpc      = true
+
+  depends_on = [
+    aws_internet_gateway.main-gateway
+  ]
+}
