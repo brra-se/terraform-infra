a72c3bc9fb6bab9d9d6d7f192c656c5389db1742
PScoriae <pierreccesario@gmail.com>
2023-03-17 00:34:55 +0800

    feat: add optional site_direct var

--- modules/s3-static-site/variables.tf
+++ modules/s3-static-site/variables.tf
@@ -12,3 +12,15 @@ variable "cloudflare_zone_id" {
   description = "Cloudflare zone id"
   type        = string
 }
+
+variable "site_redirect" {
+  description = "If provided, create another CNAME record and S3 bucket for redirection to main bucket, typically for www subdomain"
+  type = object({
+    cname_record     = string
+    s3_bucket_policy = string
+  })
+  default = {
+    cname_record     = ""
+    s3_bucket_policy = ""
+  }
+}
