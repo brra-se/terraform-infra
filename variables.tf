3fa20e4952fd39f69abeda678c0ef944234fd240
PScoriae <pierreccesario@gmail.com>
2023-03-17 01:48:24 +0800

    feat: make variables sensitive

--- variables.tf
+++ variables.tf
@@ -1,13 +1,16 @@
 variable "cloudflare_api_token" {
-  type = string
+  type      = string
+  sensitive = true
 }
 
 # pierreccesario.com
 variable "pcc_cloudflare_zone_id" {
-  type = string
+  type      = string
+  sensitive = true
 }
 
 # shuttleday.info
 variable "shuttleday_cloudflare_zone_id" {
-  type = string
+  type      = string
+  sensitive = true
 }
