23a7d71d84f512621f5124a1e6ec95135018126a
PScoriae <pierreccesario@gmail.com>
2023-03-17 00:35:27 +0800

    feat: add optional site_direct var

--- modules/s3-static-site/main.tf
+++ modules/s3-static-site/main.tf
@@ -1,5 +1,6 @@
-# Creates an S3 bucket with public access for a static website, specifically a Hugo static website.
+# Creates an S3 bucket with public access for a static website.
 # Also creates an accompanying Cloudflare CNAME DNS record.
+# Has conditional site_redirect variable that creates one redirect CNAME record and bucket
 
 terraform {
   required_providers {
@@ -14,6 +15,10 @@ terraform {
   }
 }
 
+## ---------------------------------------------------------------------------------------------------------------------
+## MAIN BUCKET
+## Standard Cloudflare and S3 resources for a static site
+## ---------------------------------------------------------------------------------------------------------------------
 resource "aws_s3_bucket" "static-site" {
   bucket = var.cname_record
 }
@@ -55,6 +60,44 @@ resource "cloudflare_record" "static-site" {
   zone_id = var.cloudflare_zone_id
   name    = var.cname_record
   value   = aws_s3_bucket_website_configuration.static-site.website_endpoint
+  type    = "CNAME"
+  proxied = true
+}
+
+## ---------------------------------------------------------------------------------------------------------------------
+## OPTIONAL REDIRECT BUCKET
+## Creates necessary resources for a site redirect if defined through site_redirect
+## ---------------------------------------------------------------------------------------------------------------------
+resource "aws_s3_bucket" "redirect" {
+  count  = var.site_redirect != { cname_record = "", s3_bucket_policy = "" } ? 1 : 0
+  bucket = var.site_redirect.cname_record
+}
+
+resource "aws_s3_bucket_policy" "redirect" {
+  count  = var.site_redirect != { cname_record = "", s3_bucket_policy = "" } ? 1 : 0
+  bucket = aws_s3_bucket.redirect[0].id
+  policy = file(var.site_redirect.s3_bucket_policy)
+}
+
+resource "aws_s3_bucket_acl" "www_shuttleday_bucket_acl" {
+  count  = var.site_redirect != { cname_record = "", s3_bucket_policy = "" } ? 1 : 0
+  bucket = aws_s3_bucket.redirect[0].id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "redirect" {
+  count  = var.site_redirect != { cname_record = "", s3_bucket_policy = "" } ? 1 : 0
+  bucket = aws_s3_bucket.redirect[0].id
+
+  redirect_all_requests_to {
+    host_name = aws_s3_bucket.static-site.bucket
+  }
+}
+resource "cloudflare_record" "redirect" {
+  count   = var.site_redirect != { cname_record = "", s3_bucket_policy = "" } ? 1 : 0
+  zone_id = var.cloudflare_zone_id
+  name    = var.site_redirect.cname_record
+  value   = aws_s3_bucket_website_configuration.redirect[0].website_endpoint
   type    = "CNAME"
   proxied = true
 }
