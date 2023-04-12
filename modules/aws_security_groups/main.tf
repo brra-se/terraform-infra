a87225b5a7393da72eeb80b8d823724bb3151fe4
PScoriae <pierreccesario@gmail.com>
2023-04-12 22:40:54 +0800

    feat: add statping

--- modules/aws_security_groups/main.tf
+++ modules/aws_security_groups/main.tf
@@ -121,6 +121,14 @@ resource "aws_security_group" "allow_uptime_kuma" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  ingress {
+    description      = "Statping TCP port"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   tags = {
     Name = "allow uptime_kuma traffic"
   }
